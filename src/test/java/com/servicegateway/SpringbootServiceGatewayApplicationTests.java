30782ae098f3be537a700af8082f9bccebf0a7c7
CarlosRojas <crojasw100@gmail.com>
2023-09-05 00:41:03 -0500

    feat: Addying gateway service

---
+++ src/test/java/com/servicegateway/SpringbootServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
